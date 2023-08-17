aee059c5a8bf37d074f09870bcfad51a9065250f
trevorputbrese <trevor.putbrese@gmail.com>
2023-08-17 13:22:58 -0400

    addedd new ruby testfile 'spec-test'

---
+++ test/spec_test.rb
@@ -0,0 +1,16 @@
+# ruby_test.rb
+
+def test_success
+    result = 0
+    expected = 0
+  
+    if result == expected
+      puts "Test passed! Result is equal to expected."
+    else
+      puts "Test failed! Result is not equal to expected."
+    end
+  end
+  
+  # Call the test
+  test_success
+  
